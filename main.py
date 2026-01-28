243fe92e9fa846b701c9c70f86656d4c668b0c1a
Sammy Hansali <sammyhansali@gmail.com>
2026-01-28 10:08:25 -0500

    playing with api

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import json
+
+import requests
+
+
 def main():
-    print("Hello from trial-tracker!")
+    API_SERVER = "https://clinicaltrials.gov/api/v2"
+    ENDPOINT = f"{API_SERVER}/studies"
+    PARAMS = {
+        "query.cond": "hair loss",
+        "pageSize": 10,
+        "sort": "StudyFirstSubmitDate:desc",  # "LastUpdatePostDate:desc",
+    }
+
+    resp = requests.get(ENDPOINT, params=PARAMS)
+    resp.raise_for_status
+    # print(resp.json())
+    with open("./test_data.json", "w") as f:
+        json.dump(resp.json(), f)
 
 
 if __name__ == "__main__":
